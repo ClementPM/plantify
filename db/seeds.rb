bc2841aeed162dde833f1ba323d225654a38ad34
Clement Perier Muzet <barceloneta08003@gmail.com>
2020-11-14 15:42:01 +0100

    seeding done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require "open-uri"
+require 'httparty'
 
 puts "Cleaning database..."
 
@@ -15,51 +16,42 @@ User.destroy_all
 
 puts "Starting seed 🌱"
 
-user1 = User.create!(email: 'jamilemarcele@me.com', password: '123456')
+names = ['Tay', 'Jamile', 'Clement', 'Martyna', 'Fabienne', 'Marin', 'Ben', 'Gus', 'Paul']
+users = []
+names.each do |name|
+  users << User.create!(email: "#{name}@gmail.com", password: 'password', name: name) 
+end
 
-photo1 = URI.open('https://images.unsplash.com/photo-1513030271585-306961d14f03?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80')
-plant1 = Plant.create!(
-name: 'Charlie',
-description: 'Charlie is a real cutie. It does not require a lot of attention',
-price: 75,
-category:'indoor',
-user: user1,
-address: 'Tokyo'
+plants = HTTParty.get(
+  'https://trefle.io/api/v1/plants',
+  query: {
+    "token": ENV['TREFLE_API_KEY']
+  }
 )
-plant1.photos.attach(io: photo1, filename: 'charlie.jpeg', content_type: 'image/jpeg')
+plants.parsed_response
 
-photo2 = URI.open('https://images.unsplash.com/photo-1508988445015-c834399679ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1713&q=80')
-plant2 = Plant.create!(
-name: 'Ben',
-description: 'Ben has succulent rosette-form leaves in shades of green, purple, and red',
-price: 50,
-category:'indoor',
-user: user1,
-address: 'Barcelona'
-)
-plant2.photos.attach(io: photo2, filename: 'ben.jpeg', content_type: 'image/jpeg')
+list = plants['data']
+list[1..20].each do |p|
+  name = p['common_name']
+  scientific_name = p['scientific_name']
+  family_common_name = p['family_common_name']
+  category = Plant::CATEGORIES.sample
+  url = ("http://geodb-free-service.wirefreethought.com/v1/geo/cities?offset=#{rand(1..24000)}&limit=1")  
+  response = HTTParty.get(url)
+  sleep(1)
+  city = response.parsed_response['data'].first['city']
 
-photo3 = URI.open('https://images.unsplash.com/photo-1575574202425-ba42a224118b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
-plant3 = Plant.create!(
-name: 'Kitty',
-description: 'Growing Kitty is not only rewarding but extremely tasty. Yummy!',
-price: 90,
-category:'outdoor',
-user: user1,
-address: 'Amsterdam'
-)
-plant3.photos.attach(io: photo3, filename: 'kitty.jpeg', content_type: 'image/jpeg')
+  p "name: #{name} city: #{city}" 
 
-photo4 = URI.open('https://images.unsplash.com/photo-1509937528035-ad76254b0356?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1524&q=80')
-plant4 = Plant.create!(
-name: 'Rosie',
-description: 'drinking cactus Rosie’s water has saved the lives of people traveling through the desert',
-price: 45,
-category:'outdoor',
-user: user1,
-address: 'Lisbon'
-)
-plant4.photos.attach(io: photo4, filename: 'rosie.jpeg', content_type: 'image/jpeg')
-
+  photo = URI.open(p['image_url'])
+  plant = Plant.create!(
+    name: name,
+    description: "Also called #{scientific_name}. Is a species of #{family_common_name}.",
+    address: city,
+    user: users.sample,
+    price: rand(10..199),
+    category: category)
+  plant.photos.attach(io: photo, filename: "#{name}.jpeg", content_type: 'image/jpeg')
+end
 
 puts "Finished seeding file 😀"
