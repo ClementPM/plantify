86900d312c69f1b0397b12c5b7fca9c55228f714
Jamile Radloff <jamilemarcele@gmail.com>
2020-11-14 12:37:10 +0100

    dashboard page styled and done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,4 @@
 class BookingsController < ApplicationController
-
   def create
     @plant = Plant.find(params[:plant_id])
     @booking = Booking.new(booking_params)
@@ -13,10 +12,23 @@ class BookingsController < ApplicationController
     end
   end
 
+  def accept
+    @booking = Booking.find(params[:id])
+    @booking.status = "accepted"
+    @booking.save
+    redirect_to dashboard_path
+  end
+
+  def reject
+    @booking = Booking.find(params[:id])
+    @booking.status = "rejected"
+    @booking.save
+    redirect_to dashboard_path
+  end
+
   private
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
-
 end
